896858502178d3cf641c329025d8853ddf18ac55
emonmahmud1 <emonmahmud083@gmail.com>
2025-06-20 22:10:33 +0600

    Update server log message and modify Vercel configuration for routing and builds

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get("/users", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
